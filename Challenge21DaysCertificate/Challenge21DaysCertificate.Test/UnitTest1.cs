6869d64ec05efc980d0b14010f32c468b9201819
YuriiYusko <yura.yusko.y@gmail.com>
2023-04-22 12:05:03 +0200

    V2

---
+++ Challenge21DaysCertificate/Challenge21DaysCertificate.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Challenge21DaysCertificate.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
